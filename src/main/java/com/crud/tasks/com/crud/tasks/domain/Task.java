9733928f8619ba49fc2cc511997af93fc42b3c10
parasolka90 <parasolka90@gmail.com>
2017-10-05 08:48:31 +0200

    14.4

--- src/main/java/com/crud/tasks/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/domain/Task.java
@@ -1,5 +1,11 @@
 package com.crud.tasks.com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
 public class Task {
     private Long id;
     private String title;
