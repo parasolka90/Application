a86466c9ea64a4d560acad8027440b0e320e7470
parasolka90 <parasolka90@gmail.com>
2017-10-25 21:36:39 +0200

    showtasks.bat

---
+++ showtasks.bat
@@ -0,0 +1,21 @@
+start runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runexplorer
+echo.
+echo RUN CRUD has errors - breaking work
+goto fail
+
+:runexplorer
+start chrome.exe http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "1" goto stoptomcat
+goto end
+
+:stoptomcat
+call %CATALINA_HOME%\bin\shutdown.bat
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
