1401f9c347abf98e444efe2b3b958eacdf1f58c9
parasolka90 <parasolka90@gmail.com>
2017-11-09 22:39:05 +0100

    mail tests

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
+
+    public Mail(String mailTo,String subject,String message){
+        this.mailTo=mailTo;
+        this.subject=subject;
+        this.message=message;
+    }
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //Given
+        Mail mail = new Mail("test@test.com","Test","Test Message","lolo@lolo.pl");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        Mockito.verify(javaMailSender,Mockito.times(1)).send(mailMessage);
+    }
+
+}
